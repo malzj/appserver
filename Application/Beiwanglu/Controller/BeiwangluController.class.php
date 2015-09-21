bec18c63fd292dd97c34800a83be9903c46fbff1
609658960@qq.com <609658960@qq.com>
2015-09-21 17:28:27 +0800

    备忘录

---
+++ Application/Beiwanglu/Controller/BeiwangluController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl
+ * Date: 2015/9/21
+ * Time: 17:19
+ */
+
+namespace Beiwanglu\Controller;
+
+
+use Think\Controller;
+
+class BeiwangluController extends Controller
+{
+public function index(){
+    $this->display();
+}
+}
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,9 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $db = D('memorandum');
-        $date['title']=1;
-        $date['content'] = "好的";
-        $db -> add($date);
+
     }
 }
