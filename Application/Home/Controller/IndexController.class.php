61b1b76df3afde1e7efc79bacbe509621a0406b2
609658960@qq.com <609658960@qq.com>
2015-09-21 17:35:56 +0800

    1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,9 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $db = D('memorandum');
-        $date['title']=1;
-        $date['content'] = "好的";
-        $db -> add($date);
+        echo "你好！";
     }
 }
---
+++ Application/User/Controller/UserController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace User\Controller;
+/**
+ * Created by PhpStorm.
+ * User: peng
+ * Date: 2015/9/21
+ * Time: 17:30
+ */
+class UserController extends Controller
+{
+
+}
