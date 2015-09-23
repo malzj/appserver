4e7beed682a904d79d6c0165f0f3a0b490d801c8
609658960@qq.com <609658960@qq.com>
2015-09-23 16:11:50 +0800

    备忘录

--- Application/Runtime/Cache/Beiwanglu/57fe7aa1d967919e4e6558ede5ff3f65.php
+++ Application/Runtime/Cache/Beiwanglu/57fe7aa1d967919e4e6558ede5ff3f65.php
@@ -1,47 +1,68 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 <head>
     <title>规划宝后台管理系统</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="${resource(dir: 'css', file: 'bootstrap.min.css')}" rel="stylesheet">
-    <link href="${resource(dir: 'css', file: 'bootstrap-reset.css')}" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/bootstrap-reset.css" rel="stylesheet">
     <!--external css-->
-    <link href="${resource(dir: 'assets/font-awesome/css', file: 'font-awesome.css')}" rel="stylesheet">
-    <link href="${resource(dir: 'assets/jquery-easy-pie-chart', file: 'jquery.easy-pie-chart.css')}" rel="stylesheet">
-    <link href="${resource(dir: 'css', file: 'owl.carousel.css')}" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css " rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/owl.carousel.css" rel="stylesheet">
 
     <!--right slidebar-->
-    <link href="${resource(dir: 'css', file: 'slidebars.css')}" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/slidebars.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="${resource(dir: 'css', file: 'styleone.css')}" rel="stylesheet">
-    <link href="${resource(dir: 'css', file: 'style-responsive.css')}" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/styleone.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/style-responsive.css" rel="stylesheet">
 
-    <link href="${resource(dir: 'css', file: 'ownset.css')}" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/ownset.css" rel="stylesheet">
 </head>
 
 <body>
 
 <section id="container" >
     <!--header start-->
-    <include file="./header" />
+    
+<header class="header reset" style="background: #1c7ff4;">
+    <!--logo start-->
+    <a class="logo"><img height="30" src="/appserver1/Public/guihuabao/img/logo_1.png"/></a>
+    <!--logo end-->
+</header>
 
 
     <!--header end-->
     <!--sidebar start-->
-    <include file="./siderbar" />
+    <aside>
+    <div id="sidebar"  class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu reset mt80" id="nav-accordion">
+            <li>
+                <a>
+                    <i class="fa user"></i>
+                    <span>备忘录</span>
+                </a>
+            </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
     <!--sidebar end-->
     <!--main content start-->
     <section id="main-content">
         <section class="wrapper mt80">
-            <form class="form-horizontal tasi-form" url="[controller:'login',action:'toolContentSave']" method="post"  enctype= "multipart/form-data">
+            <form class="form-horizontal tasi-form" action="<?php echo U('Beiwanglu/beiwangluAdd');?>" method="post"  enctype= "multipart/form-data">
 
                 <div class="hxzs_heading clearfix">
-                    %{--<header class="panel-heading">--}%
+
                     <h2>新建</h2>
+                    <a href="<?php echo U('Beiwanglu/index',array('cid'=>$_SESSION['cid'],'uid'=>$_SESSION['uid'],'companyappid'=>$_SESSION['companyappid']));?>" class="btn btn-info" style="display:block;float:right;">返回列表</a>
+
                     <button type="submit" class="btn btn-info f-r">保存</button>
-                    %{--</header>--}%
+
 
                 </div>
                 <!--<g:hiddenField name="toolId" value="${toolId}"></g:hiddenField>-->
@@ -53,24 +74,13 @@
                         </div>
                     </div>
                     <div class="textarea">
-                        <textarea id="editor_id" name="content" style="width:100%;height:500px;"></textarea>
+                        <textarea  name="content" style="width:100%;height:500px;"></textarea>
                     </div>
 
                 </div>
             </form>
         </section>
         <!--main content end-->
-
-        <!--footer start-->
-        %{--<footer class="site-footer">--}%
-        %{--<div class="text-center">--}%
-            %{--2013 &copy; FlatLab by VectorLab.--}%
-            %{--<a href="index.html#" class="go-top">--}%
-                %{--<i class="fa fa-angle-up"></i>--}%
-                %{--</a>--}%
-            %{--</div>--}%
-        %{--</footer>--}%
-        <!--footer end-->
     </section>
 
     <!-- js placed at the end of the document so the pages load faster -->
--- Application/Runtime/Cache/Beiwanglu/ebfc9f8a076019799537e41c145dc0dc.php
+++ Application/Runtime/Cache/Beiwanglu/ebfc9f8a076019799537e41c145dc0dc.php
@@ -1,10 +1,104 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title></title>
+    <title>规划宝后台管理系统</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/appserver1/Public/guihuabao/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="/appserver1/Public/guihuabao/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css " rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/owl.carousel.css" rel="stylesheet">
+
+    <!--right slidebar-->
+    <link href="/appserver1/Public/guihuabao/css/slidebars.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/appserver1/Public/guihuabao/css/styleone.css" rel="stylesheet">
+    <link href="/appserver1/Public/guihuabao/css/style-responsive.css" rel="stylesheet">
+
+    <link href="/appserver1/Public/guihuabao/css/ownset.css" rel="stylesheet">
 </head>
+
 <body>
-asdf
+
+<section id="container" >
+    <!--header start-->
+    
+<header class="header reset" style="background: #1c7ff4;">
+    <!--logo start-->
+    <a class="logo"><img height="30" src="/appserver1/Public/guihuabao/img/logo_1.png"/></a>
+    <!--logo end-->
+</header>
+
+
+    <!--header end-->
+    <!--sidebar start-->
+    <aside>
+    <div id="sidebar"  class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu reset mt80" id="nav-accordion">
+            <li>
+                <a>
+                    <i class="fa user"></i>
+                    <span>备忘录</span>
+                </a>
+            </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+    <!--sidebar end-->
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper mt80">
+            <div class="row">
+                <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading">
+                            <h2 style="float:left;">备忘录列表</h2>
+                            <a href="<?php echo U('Beiwanglu/create');?>" class="btn btn-info" style="display:block;float:right;">新建</a>
+                            <div style="clear:both;"></div>
+                        </header>
+                        <table class="table table-striped table-advance table-hover">
+                            <thead>
+                            <tr>
+                                <th width="10%">编号</th>
+                                <th width="70%">标题</th>
+                                <th>操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><tr class="${(i % 2) == 0 ? 'even' : 'odd'}">
+                                    <td><?php echo ($vo["id"]); ?></td>
+                                    <td><a href="<?php echo U('Beiwanglu/show','id='.$vo['id']);?>"><?php echo ($vo["title"]); ?></a></td>
+                                    <td>
+                                        <a href="<?php echo U('Beiwanglu/show','id='.$vo['id']);?>" class="btn btn-success btn-xs"><i class="fa fa-eye"></i></a>
+                                        <a href="<?php echo U('Beiwanglu/update','id='.$vo['id']);?>" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
+                                        <a href="<?php echo U('Beiwanglu/beiwangluDelete','id='.$vo['id']);?>" class="btn btn-danger btn-xs" onclick="return confirm('确定删除？');"><i class="fa fa-trash-o "></i></a>
+                                    </td>
+                                </tr><?php endforeach; endif; else: echo "" ;endif; ?>
+
+                            </tbody>
+                        </table>
+                        <div class="pagination">
+                           <!--<?php echo ($page); ?>-->
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+
+        </section>
+    </section>
+    <!--main content end-->
+</section>
+
+
+
+
+
 </body>
 </html>
